2d0653875feec461c7c95b28403ce95b1874fd8e
psrikaavya9 <psrikaavya@gmail.com>
2026-04-06 22:46:38 +0530

    restore dashboard

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,3 +1,5 @@
-export default function Page() {
-  return <h1>App Working ✅</h1>;
+import { redirect } from "next/navigation";
+
+export default function DashboardRootPage() {
+  redirect("/dashboard");
 }
